755410da35d57127d23d1089b87af0413de5c260
nealdeters <nealdeters@gmail.com>
2015-04-16 22:34:34 -0500

    added descriptions and images

---
+++ app/assets/javascripts/nav-toggle.js
@@ -0,0 +1,6 @@
+$(function() {
+	$('.nav navbar-nav a').on('click', function(){
+    	$(".button").click(); //bootstrap 2.x
+    	$(".navbar-toggle").click() //bootstrap 3.x by Richard
+	});
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "My Location - Lincoln Park Office - Chicago, IL 60614"
+    base_title = "MyFlowerShop - Lincoln Park Office - Chicago, IL 60614"
     if page_title.empty?
       base_title
     else
