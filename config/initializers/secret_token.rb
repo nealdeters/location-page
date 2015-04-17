6c45d2f57c587a783133ccbf35b54c80688de689
nealdeters <nealdeters@gmail.com>
2015-04-16 19:05:17 -0500

    html and css loaded

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocationPage::Application.config.secret_key_base = '25184a4a4caf6cd7e314db32628d5fda64d15c3c44dcf4091b9a86eaa34f8262d7a3a87a3c1d042ce28ea010b1d416e41da27e8c901f4fe19674ee086fbb0ac3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LocationPage::Application.config.session_store :cookie_store, key: '_location-page_session'
