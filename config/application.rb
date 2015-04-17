5baf8741f4353f1461f6e88baea30d6cf321bcc8
nealdeters <nealdeters@gmail.com>
2015-04-16 19:41:47 -0500

    more adjustments

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module LocationPage
   class Application < Rails::Application
   
   	config.assets.initialize_on_precompile = false
+  	config.serve_static_assets = true
   	
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
